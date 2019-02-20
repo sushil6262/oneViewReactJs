0e92778c917aae364c641c5cb52eea5f4fa82a7d
sushil6262 <aadisrma26@gmail.com>
2019-02-20 14:40:14 +0530

    code added

--- src/Component/ParentComponent/Parent.js
+++ src/Component/ParentComponent/Parent.js
@@ -8,6 +8,8 @@ import Stripe from '../../images/credit-cards.svg'
 import QrCode from '../../images/qrcode.svg'
 import Nvest from '../../images/NVEST.svg'
 
+const ReactCopyButtonWrapper = require('../../../node_modules/react-copy-button-wrapper')
+
 
 class Parent extends Component {
     state = {
@@ -17,6 +19,7 @@ class Parent extends Component {
         fourthLayoutAnimation: true,
         fifthLayoutAnimation: true,
         sixthLayoutAnimation: true,
+        copyText:true
     }
     firstLayoutAnimation = () => {
         this.setState({
@@ -59,7 +62,12 @@ class Parent extends Component {
 
         })
     }
-
+    copyText=()=>{
+        this.setState({
+            copyText:false
+        })
+    }
+    
     render() {
         return (
             <div>
@@ -71,7 +79,7 @@ class Parent extends Component {
                             <p className="heading2nd companyTextColor">Your Passport To The Global X Change<br /></p>
 
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-4 tokenValueHide">
                             <div className="row customRow">
                                 <div className="col-md-9 col-lg-10 col-xl-8 customBlue">
                                     <div className="blueBox">
@@ -81,33 +89,33 @@ class Parent extends Component {
                                             <hr className="custmHR" />
                                         </div>
                                         <div className="tokenBx">
-                                            <p className="Customtitle"><strong>Token Name </strong></p>
-                                            <p className="Custompara">Global Exchange</p>
+                                            <p className="Customtitle"><strong>Token Symbol</strong></p>
+                                            <p className="Custompara">GXT</p>
                                             <hr className="custmHR" />
                                         </div>
                                         <div className="tokenBx">
-                                            <p className="Customtitle"><strong>Token Name </strong></p>
-                                            <p className="Custompara">Global Exchange</p>
+                                            <p className="Customtitle"><strong>Token Total Supply</strong></p>
+                                            <p className="Custompara">350000000</p>
                                             <hr className="custmHR" />
                                         </div>
                                         <div className="tokenBx">
-                                            <p className="Customtitle"><strong>Token Name </strong></p>
-                                            <p className="Custompara">Global Exchange</p>
+                                            <p className="Customtitle"><strong>Token Phase Name</strong></p>
+                                            <p className="Custompara">GXT Phase 1</p>
                                             <hr className="custmHR" />
                                         </div>
                                         <div className="tokenBx">
-                                            <p className="Customtitle"><strong>Token Name </strong></p>
-                                            <p className="Custompara">Global Exchange</p>
+                                            <p className="Customtitle"><strong>Token Phase Supply</strong></p>
+                                            <p className="Custompara">100000000</p>
                                             <hr className="custmHR" />
                                         </div>
                                         <div className="tokenBx">
-                                            <p className="Customtitle"><strong>Token Name </strong></p>
-                                            <p className="Custompara">Global Exchange</p>
+                                            <p className="Customtitle"><strong>Token Phase Price</strong></p>
+                                            <p className="Custompara">0.0006 ETH</p>
                                             <hr className="custmHR" />
                                         </div>
                                         <div className="tokenBx">
-                                            <p className="Customtitle"><strong>Token Name </strong></p>
-                                            <p className="Custompara">Global Exchange</p>
+                                            <p className="Customtitle"><strong>Token Sold</strong></p>
+                                            <p className="Custompara">1360 / 100000000</p>
                                         </div>
                                     </div>
                                 </div>
@@ -116,10 +124,32 @@ class Parent extends Component {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row" style={{height:"40px"}}>
-                            <div className="col"></div>
+                        <div className="row customRow mobileDisplay">
+                            <div>
+                                <div className="DRcell">
+                                    <div className="col-xs-4">
+                                        <p className="DRtext companyTextColor">Token Phase Supply</p>
+                                        <p className="DRNUmber">100000000</p>
+                                    </div>
+                                </div>
+                                <div className="DRcell">
+                                    <div className="col-xs-4">
+                                        <p className="DRtext companyTextColor">Token Phase Price</p>
+                                        <p className="DRNUmber">0.0006 ETH</p>
+                                    </div>
+                                </div>
+                                <div className="DRcell">
+                                    <div className="col-xs-4">
+                                        <p className="DRtext companyTextColor">Token Sold</p>
+                                        <p className="DRNUmber">1360 / 100000000</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                    </div>
+                    <div className="row" style={{ height: "40px" }}>
+                        <div className="col"></div>
+                    </div>
                     <div className="">
                         <div className="customDownNavigation" onClick={this.firstLayoutAnimation}>
                             <p className="custumPara companyTextColor">Click To Proceed</p>
@@ -133,23 +163,22 @@ class Parent extends Component {
                     <div className="inputContent">
                         <h1 className="companyTextColor companyTextFontWeight">Step: 1</h1>
                         <form>
-                            <div className="form-group"><label className="companyTextColor">How Many GXTokens Would You Like To Purchase?                            &nbsp; &nbsp; &nbsp; &nbsp;
-                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                            &nbsp; &nbsp; &nbsp;<br /></label><input className="form-control" type="text"
+                            <div className="form-group"><p className="companyTextColor textCenter">How Many GXTokens Would You Like To Purchase?                            &nbsp; &nbsp; &nbsp; &nbsp;
+                                    <br /></p><input className="form-control" type="text"
                                     placeholder="Enter Number of Tokens" /></div>
                         </form>
                         <p>Total Price (ETH) : </p>
                         <p>Total Price ($) : </p>
-                        <div className="row" style={{height:"40px"}}>
+                        <div className="row" style={{ height: "40px" }}>
                             <div className="col"></div>
                         </div>
                         <div className="">
-                                <div className="customDownNavigation" onClick={this.secoundLayoutAnimation}>
-                                    <p className="custumPara companyTextColor">Click To Proceed with Payment</p>
-                                    <p className="custumPara"><i className="fas fa-arrow-down companyTextColor"></i></p>
-                                </div>
+                            <div className="customDownNavigation" onClick={this.secoundLayoutAnimation}>
+                                <p className="custumPara companyTextColor">Click To Proceed with Payment</p>
+                                <p className="custumPara"><i className="fas fa-arrow-down companyTextColor"></i></p>
                             </div>
-                        
+                        </div>
+
                     </div>
                 </div>
                 <div className={this.state.thirdLayoutAnimation ? "ThirdLayout" : "ThirdLayoutAnimation"}><img src={Logo} className="imglogo" alt="logo" />
@@ -158,7 +187,7 @@ class Parent extends Component {
                         <p className="Customtext2 companyTextColor">How Do You Want To Pay?<br /></p>
                         <div className="container" style={{ marginTop: "-40px" }}>
                             <div className="card-group">
-                                <div className="card customCard" onClick={this.thirdLayoutAnimation}><img className="card-img-top w-100 d-block paymentImg"
+                                <div className="card customCard displayHide" onClick={this.thirdLayoutAnimation}><img className="card-img-top w-100 d-block paymentImg"
                                     src={MetaMask} alt="Metamask" />
                                     <div className="card-body cardBody">
                                         <h4 className="card-title Customtext">Metamask</h4>
@@ -176,7 +205,7 @@ class Parent extends Component {
                                         <h4 className="card-title Customtext">Bitcoin</h4>
                                     </div>
                                 </div>
-                                <div className="card customCard" onClick={this.thirdLayoutAnimation}><img className="card-img-top w-100 d-block paymentImg"
+                                <div className="card customCard displayHide" onClick={this.thirdLayoutAnimation}><img className="card-img-top w-100 d-block paymentImg"
                                     src={Nvest} alt="Nvest" />
                                     <div className="card-body cardBody">
                                         <h4 className="card-title Customtext">NvestBank</h4>
@@ -196,12 +225,11 @@ class Parent extends Component {
                     <div className="inputContent">
                         <h1 className="companyTextColor companyTextFontWeight">Step: 3</h1>
                         <form>
-                            <div className="form-group"><label className="companyTextColor">Where Should We Send The GXT? &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                            &nbsp; &nbsp;<br /></label><input className="form-control" type="text"
+                            <div className="form-group"><p className="companyTextColor textCenter">Where Should We Send The GXT?
+                           <br /></p><input className="form-control" type="text"
                                     placeholder="Enter An ERC20 Compatible Address" /></div>
                         </form>
-                        <div className="row" style={{height:"40px"}}>
+                        <div className="row" style={{ height: "40px" }}>
                             <div className="col"></div>
                         </div>
                         <div className="row customRow">
@@ -217,9 +245,15 @@ class Parent extends Component {
                 <div className={this.state.fifthLayoutAnimation ? "fifthLayout" : "fifthLayoutAnimation"}><img src={Logo} className="imglogo" alt="logo" />
                     <div className="inputContent">
                         <h1 className="companyTextColor companyTextFontWeight">Step: 4</h1>
-                        <p className="companyTextColor">Send (dyname value should be here)Ethereum To This Address</p>
-                        <p className="tokenValuestyle">0xa969c1D98e4754Eff010A6daE217dC5c4a9c24aA</p><img src={QrCode} className="orcode" alt="logo" />
-                        <div className="row" style={{height:"40px"}}>
+                        <p className="companyTextColor textCenter">Send (dyname value should be here)Ethereum To This Address</p>
+                        <div  onClick={this.copyText}>
+                        <ReactCopyButtonWrapper text="0xa969c1D98e4754Eff010A6daE217dC5c4a9c24aA">
+                            <p className="tokenValuestyle textJumble hint--bottom" aria-label={this.state.copyText?"Click to Copy":"Copied"}>0xa969c1D98e4754Eff010A6daE217dC5c4a9c24aA</p>
+                            
+                        </ReactCopyButtonWrapper>
+                        </div>
+                        <img src={QrCode} className="orcode" alt="logo" />
+                        <div className="row" style={{ height: "40px" }}>
                             <div className="col"></div>
                         </div>
                         <div className="row customRow">
@@ -236,12 +270,11 @@ class Parent extends Component {
                     <div className="inputContent">
                         <h1 className="companyTextColor companyTextFontWeight">Step: 5</h1>
                         <form>
-                            <div className="form-group"><label className="companyTextColor">Help Us Validate Your Purchase &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                            &nbsp; &nbsp;<br /></label><input className="form-control" type="text"
+                            <div className="form-group"><p className="companyTextColor textCenter">Help Us Validate Your Purchase
+                                    <br /></p><input className="form-control" type="text"
                                     placeholder="Enter the txnHASH" /></div>
                         </form>
-                        <div className="row" style={{height:"40px"}}>
+                        <div className="row" style={{ height: "40px" }}>
                             <div className="col"></div>
                         </div>
                         <div className="row customRow">
